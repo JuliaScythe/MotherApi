168627fc42fb10f6468e8b7122440cd1558e24b7
Jake Irvine <me@jakeirvine.com>
2015-08-10 13:40:14 +0100

    Begun to add google maps android support

--- API.py
+++ API.py
@@ -1,5 +1,11 @@
 import constants
 
+class Func:
+    def __init__(self, name, desc, identifyer):
+        self.name = name
+        self.desc = desc
+        self.identifyer = identifyer
+
 class API:
     name = ""
     version = 0
@@ -7,9 +13,18 @@ class API:
     auth = 0
     mode = 0
     funcs = []
-    def __init__(self, name, version, language, mode=constants.REST, auth=constants.NONE):
+    def __init__(self, name, version, letter, language,  mode=constants.REST, auth=constants.NONE):
         self.name = name
         self.version = version
         self.language = language
         self.mode = mode
         self.auth = auth
+        self.letter = letter
+    def registerFunc(self, name, desc, identifyer):
+        self.funcs.append(Func(name, desc, identifyer))
+
+    def printFuncs(self):
+        i = "a"
+        for f in self.funcs:
+            print("["+ i + "] | " + f.name.ljust(30) + "| " + f.desc.ljust(50) + " | " + f.identifyer)
+            i = chr(ord(i) + 1)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests as r
 import constants
 import sys
@@ -7,9 +8,15 @@ from API import API
 version = "version 0.01"
 
 # API variebles
-googleMaps = API("Google Maps API", 1.0, constants.JAVASCRIPT, constants.EMBED, constants.API_KEY)
-
-
+apis = []
+googleMaps = API("Google Maps API", 1.0, "a", constants.JAVASCRIPT, constants.EMBED, constants.API_KEY)
+apis.append(googleMaps)
+googleMaps.registerFunc("Standard Map", "The standard google map", "googleMaps\\android\\hello.java")
+googleMaps.registerFunc("Std. Map w/ marker", "Map with custom marker text", "googleMaps\\android\\hellomarker.java")
+googleMaps.registerFunc("Map types", "Map with choice of types", "googleMaps\\android\\type.java")
+googleMaps.registerFunc("Map w/ custom markers", "Map with custom marker image", "googleMaps\\android\\markerimage.java")
+googleMaps.registerFunc("Map w/ flat markers", "Map with flat marker images. These rotate with prespective.", "googleMaps\\android\\markerflat.java")
+googleMaps.printFuncs()
 
 def startup():
     print("motherApi")
@@ -19,7 +26,18 @@ def startup():
     try:
         res = r.get("http://www.google.com")
     except("*"):
-        print("Error 0x01: No internet detected")
+        print("Error 101: No internet detected")
         sys.exit(1)
     print("Connection Succesful")
+
+def listApis():
+    for a in apis:
+        print("["+ a.letter + "] | " + a.name.ljust(20) + "| " + str(a.version))
+    ch = input("Choose an API >>>  ")
+    for a in apis:
+        if ch == a.letter:
+            return a
+    print("Please choose am API")
+    listApis
 startup()
+listApis()
